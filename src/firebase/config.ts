de310e250a5b2058456fd73b680c67df63113384
Anurag Gupta <anurag.sculptexpert@gmail.com>
2026-04-05 18:03:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6866268644-3e267",
+  "appId": "1:727566050822:web:0ede594a47b86106618587",
+  "apiKey": "AIzaSyAPmf5n0B7Rcnzxr-Uwo6SULLqCmv6syRA",
+  "authDomain": "studio-6866268644-3e267.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "727566050822"
+};
